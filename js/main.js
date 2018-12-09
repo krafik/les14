ec48c746031ff1b37bd3c40e119c2c5590e0c9c9
krafik <alexeym4l@mail.ru>
2018-12-09 22:04:13 +0300

    les14

---
+++ js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+$(document).ready(function () {
+	$('[href="#sheldure"]').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideDown("slow");
+	});
+	$('.main_btn').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+		$('.modal').slideDown("slow");
+	});
+	$('.main_btna').on('click', () => {
+		$('.overlay').fadeToggle('slow');
+    $('.modal').slideDown("slow");
+  });
+  $('.close').on('click', () => {
+    $('.overlay').fadeToggle('slow');
+    $('.modal').slideUp("slow");
+  });
+});
